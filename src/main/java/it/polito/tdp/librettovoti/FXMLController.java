e08ed2de4e8755d9368d80c48f39e87e62e4416b
Mattia <Mattia@LAPTOP-3K6IEHAJ>
2021-03-30 10:19:39 +0200

    modifiche

--- src/main/java/it/polito/tdp/librettovoti/FXMLController.java
+++ src/main/java/it/polito/tdp/librettovoti/FXMLController.java
@@ -2,24 +2,54 @@ package it.polito.tdp.librettovoti;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.librettovoti.model.Libretto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController {
+	
+	private Libretto model;
+
     @FXML
-    private Label label;
-    
+    private ResourceBundle resources;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private URL location;
+
+    @FXML
+    private TextField txtVoto;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtData;
+
+    @FXML
+    private Button btnInserisci;
+
+    @FXML
+    void handleInserisci(ActionEvent event) {
+    	
+    	
+    	
+    	
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+    @FXML
+    void initialize() {
+        assert txtVoto != null : "fx:id=\"txtVoto\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtData != null : "fx:id=\"txtData\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
+
+	public void setModel(Libretto model) {
+		// TODO Auto-generated method stub
+		this.model = model;
+	}
 }
